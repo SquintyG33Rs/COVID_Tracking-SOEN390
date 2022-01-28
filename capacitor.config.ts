ad7caa2eed1afcb0c877905b5f3e820b70bdfa0e
SquintyG33Rs <squentind@gmail.com>
2022-01-27 20:32:42 -0500

    Change from React to Angular
    wasted 1h30min fixing node... hope it was worth it

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'COVID_Tracking-SOEN390',
-  webDir: 'build',
+  webDir: 'www',
   bundledWebRuntime: false
 };
 
