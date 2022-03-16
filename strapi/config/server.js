5f8d0c08cf3290ae0c5ed76efdb6e83ce0ef075f
PhunnieB <phunniehypnotix@gmail.com>
2022-03-16 13:40:00 -0400

    include db

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '98adad9f74d00134d9313c7e68546c97'),
+    },
+  },
+});
