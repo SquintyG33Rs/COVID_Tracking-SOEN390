efc86b840866e5c4f291b2eb8f1dac89a06a9250
MahHairy <MahHairy@hotmail.com>
2022-03-18 13:05:53 -0400

    - Update.ts entity created - status-update.page.ts with endpoints - Status updates are shown in the database

---
+++ src/app/entities/Update.ts
@@ -0,0 +1,48 @@
+import {AccountType} from './AccountType';
+import {FormControl} from '@angular/forms';
+
+export class Update {
+  static allUpdates: Update[] = [];
+  date: Date;
+  temp: number;
+  weight: number;
+  cough: boolean;
+  head: boolean;
+  throat: boolean;
+  fever: boolean;
+  taste: boolean;
+  tired: boolean;
+
+  constructor(date: Date,
+  temp: number,
+  weight: number,
+  cough: boolean,
+  head: boolean,
+  throat: boolean,
+  fever: boolean,
+  taste: boolean,
+  tired: boolean)
+  {
+    this.date = date;
+    this.temp = temp;
+    this.weight = weight;
+    if (cough !== true)
+      {cough  = false;}
+    this.cough = cough;
+    if (head !== true)
+    {head  = false;}
+    this.head = head;
+    if (throat !== true)
+    {throat  = false;}
+    this.throat = throat;
+    if (fever !== true)
+    {fever  = false;}
+    this.fever = fever;
+    if (taste !== true)
+  {taste  = false;}
+    this.taste = taste;
+    if (tired !== true)
+  {tired  = false;}
+    this.tired = tired;
+  }
+}
--- src/app/entities/User.ts
+++ src/app/entities/User.ts
@@ -1,7 +1,7 @@
-import { AccountType } from "./AccountType";
+import { AccountType } from './AccountType';
 import { Observable } from 'rxjs';
-
-export class InformationEvent 
+/*
+export class InformationEvent
 {
   username: string
   password: string
@@ -12,7 +12,7 @@ export class InformationEvent
   email: string
   address: string
 }
-
+*/
 export class User
 {
   static allUsers: User[] = [];
@@ -32,7 +32,7 @@ export class User
                 accountType: AccountType,
                 telephone: string,
                 email: string,
-                address: string) 
+                address: string)
   {
     this.username = username;
     this.password = password;
